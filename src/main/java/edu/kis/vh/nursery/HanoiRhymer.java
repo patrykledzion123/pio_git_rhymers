051fa414d971632f25eb5a7c789cce1a7bd4506d
Patryk Ledzion <236412@edu.p.lodz.pl>
2023-02-04 20:08:31 +0100

    2. Zmiana formatowania

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,19 @@
 package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
+    int totalRejected = 0;
 
-int totalRejected = 0;
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    //wiersze 4, 11, 13, 14 były źle sformatowane
+    //Kombinacje alt+<- oraz alt+-> przełączają między otwartymi kartami (w środowisku intelliJ)
 }
